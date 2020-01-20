5e1a238544be9d4f48a29e5630a7e23f12d62485
recruit <phindileportia20@gmail.com>
2020-01-20 11:44:15 +0200

    Commit

--- src/main/java/Password.java
+++ src/main/java/Password.java
@@ -1,79 +1,105 @@
+import java.util.*;
+public class Password {
 
-    import java.util.Scanner;
-    public class Password {
 
-        public boolean passwordIsValid(String password) {
-            Scanner input = new Scanner(System.in);
-            System.out.print("Enter password: ");
-            password = input.nextLine();
-            boolean hasDigit = false;
-            boolean hasUppercase = false;
-            boolean hasLowercase = false;
-            boolean length =false;
-            boolean exist = true;
-            boolean special_character=false;
-            for (int i = 0; i < password.length(); i++) {
-                if (password.isBlank()) {
-                    exist=false;
-                }else {
-                    System.out.println("Password should exist.");
-                }
-                if (password.length() >8 || password.length()==8) {
-                    length=true;
-                }else {
-                    System.out.println("Password should be at least 8 characters long.");
-                }
-                char Password = password.charAt(i);
-                if (Character.isDigit(Password)) {
-                    hasDigit = true;
-                }else {
-                    System.out.println("Password should have at least one number.");
-                }
-                if (Character.isUpperCase(Password)) {
-                    hasUppercase = true;
-                }else {
-                    System.out.println("Password should have at least one uppercase letter.");
-                }
-                if (Character.isLowerCase(Password)) {
-                    hasLowercase = true;
-                }else {
-                    System.out.println("Password should have at least one lowercase letter.");
-                }
-                if (!Character.isLetter(Password)&!Character.isDigit(Password)){
-                    special_character = true;
-                }else {
-                    System.out.println("Password should have at least one special character.");
-                }
-                if (hasDigit & hasLowercase & hasUppercase & exist& length & special_character) {
-                    System.out.println("correct, you've met all the conditions required.");
-                    return true;
-                }
+    boolean PasswordExist(String password) {
+        return password.length() >= 1;
+    }
+
+    boolean PasswordLength (String password) {
+        return password.length() >8;
+    }
+
+    boolean PasswordHasLowercase (String password) {
+        for (int i =1; i <=password.length(); i++) {
+            if (Character.isLowerCase(password.charAt(i))) {
+                return true;
             }
-            System.out.println("Incorrect, you did not meet one or more of the required conditions.");
-            return false;
         }
-        public boolean PasswordIsOk(){
-            Scanner scan = new Scanner(System.in);
-            System.out.print("Enter password: ");
-            String password = scan.nextLine();
-            boolean length = false;
-            boolean exist = true;
-            for (int j = 0; j< password.length(); j++){
-                if (password.length()>8 || password.length()==8){
-                    length = true;
-                }
-                else if (password.isBlank()){
-                    exist = false;
-                }
-                if (length & exist){
-                    System.out.println("correct, you've met at least 3 required conditions.");
-                    return true;
-                }
+        return false;
+    }
+
+    boolean PasswordHasUppercase (String password) {
+        for (int i = 0; i < password.length(); i++) {
+            if (Character.isUpperCase(password.charAt(i))) {
+                return true;
             }
-            System.out.println("You need to meet at least three conditions with conditions 1 & 2 included.");
-            return false;
         }
+        return false;
+    }
+
+    boolean PasswordHasDigit (String password) {
+        for (int i = 0; i < password.length(); i++) {
+            if (Character.isDigit(password.charAt(i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    boolean PasswordHasSpecialCharacter (String password) {
+        for (int i = 0; i < password.length(); i++) {
+            if (!Character.isLetterOrDigit(password.charAt(i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+   public void PasswordIsValid() {
+       System.out.println("Enter password.");
+       Scanner sc = new Scanner(System.in);
+       String password = sc.nextLine();
+
+        if (!PasswordExist(password)) {
+            System.out.println(" Password should exist");
+        }
+        if (!PasswordLength(password)) {
+            System.out.println("Password should be longer than 8 characters");
+        }
+        if (!PasswordHasLowercase(password)) {
+            System.out.println("Password should contain at least 1 lowercase letter");
+        }
+        if (!PasswordHasUppercase(password)) {
+            System.out.println("Password should contain at least 1 uppercase letter");
+        }
+        if (!PasswordHasDigit(password)) {
+            System.out.println("Password should contain at least one digit");
+        }
+        if (!PasswordHasSpecialCharacter(password)) {
+            System.out.println("Password should contain at least one special character");
+        }
+    }
+
+    public boolean PasswordIsOk(String password) {
+        int counter = 0;
+
+        if (PasswordExist(password)) {
+            counter++;
+        }
+        if (PasswordLength(password)) {
+            counter++;
+        }
+        if (PasswordHasLowercase(password)) {
+            counter++;
+        }
+        if (PasswordHasUppercase(password)) {
+            counter++;
+        }
+        if (PasswordHasDigit(password)) {
+            counter++;
+        }
+        if (PasswordHasSpecialCharacter(password)) {
+            counter++;
+        }
+
+        if (!(PasswordLength(password) && PasswordLength(password))) {
+            System.err.println("Password is never accepted if at least 3 conditions are not met.");
+        }
+
+        return counter >= 3;
     }
 
 
 
+}
